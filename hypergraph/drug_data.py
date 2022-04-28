659e6fbcecf785fb1c8db9a5cc884d7eaf89d1fb
chrisDev-2B <chrisdenaro20+gitHub@gmail.com>
2022-04-28 13:23:47 -0400

    Added numpy docstrings to functions
    Previously, there were unformatted docstrings for this class.
    Now, each function has a numpy docstring to describe the behavior of the function.

--- hypergraph/drug_data.py
+++ hypergraph/drug_data.py
@@ -23,6 +23,20 @@ class Drug_Data:
         ##############################
 
     def create_drug_table(self):
+        """This function creates a pd dataframe that holds drug-treated
+        expression levels. Data is pulled from self.raw_dataframe_dict
+        which is a dictionary where keys are sheet names and values are
+        pd dataframes of those sheets
+        
+        Returns
+        -------
+        pd dataframe
+            function merges all of the dataframes in self.raw_dataframe_dict
+            according to the first column - clone_id
+            Format of the final dataframe is: first col is gene clone_ids
+            rows correspond to specific gene, cols correspond to drug
+            entries are relative gene expression levels (to control)
+        """
         # this solution is following a stack overflow post
         # https://stackoverflow.com/questions/53935848/how-to-merge-all-data-frames-in-a-dictionary-in-python
         # 'on' determines which col we use a the key for the merge,
@@ -32,6 +46,15 @@ class Drug_Data:
         return collated_table 
 
     def get_drug_list(self):
+        """This function simply gets the sheet names from the dictionary
+        
+        Returns
+        -------
+        list
+            the return list contains all of the keys from the raw_dataframe_dict
+            (the keys are the names of the individual sheets) Each sheet contains
+            gene expression levels for treatment with a single drug
+        """
         # for now, we will treat drugs at different doses as different treatments
         # we just iterate over the keys in the dict and append them to a list
         drug_list = []
@@ -40,6 +63,16 @@ class Drug_Data:
         return drug_list
 
     def get_num_replicates(self):
+        """this function takes in a dictionary of dataframes and returns
+        the number of columns for each dataframe in the dictionary
+        Columns correspond to treatment with the drug (sheet name)
+        some experiments (treatments) contain multiple replicates
+        
+        Returns
+        -------
+        list
+            list containing the number of replicates for each sheet
+        """
         # this function will take in the dictionary of sheets and
         # return a list that holds the number of replicates for each drug
         num_replicates = []
@@ -50,6 +83,16 @@ class Drug_Data:
         return num_replicates
 
     def create_average_drug_table(self):
+        """the function gives a dataframe containing average drug effect
+        we are averaging over the replicates for each drug
+        
+        Returns
+        -------
+        dataframe
+            dataframe where rows correspond to gene clone_id and cols
+            correspond to drugs. Entries are average (over replicates)
+            effect of drug on specific gene
+        """
         # create a new dataframe that we will populate with average values
         # we start by setting the first col to the ID_REF of the existing table
         average_drug_table = pd.DataFrame([self.drug_table.ID_REF]).transpose()
@@ -68,6 +111,27 @@ class Drug_Data:
         return average_drug_table
 
     def create_significance_filters(self, drug_dataframe, significance_cutoff):
+        """this function creates a dataframe where rows correspond to gene id
+        and cols correponds to treatment, entries are true/false depending on
+        whether gene expression level in drug_dataframe is greater than
+        significance_cutoff (in abs values)
+        
+        Parameters
+        ----------
+        drug_dataframe : pd dataframe
+            dataframe where first col is clone_id, cols correspond to drugs
+            rows correspond to genes, entries are relative gene expression
+            levels
+        significance_cutoff : double
+            value for which we say a drug has a significant effect on a gene
+        
+        Returns
+        -------
+        dataframe
+            values of dataframe are True/False - depending on if values are
+            above/below the significance level
+            first column in clone_ids
+        """
         # drug_dataframe will be a dataframe that has the first col
         # as ID_REF and the rest of the columns will hold gene expression
         # levels
