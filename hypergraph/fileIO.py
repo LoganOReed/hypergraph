bde593bc16cae850a31660153f13527099e2d9d0
chrisDev-2B <chrisdenaro20+gitHub@gmail.com>
2022-04-28 19:55:58 -0400

    Added functions facilitate/fetch kgml files for KEGG pathways
    Added one main function that fetches kgml files from KEGG and stores in a new local folder in input_files called KEGG_data.
    
    Created helper functions that facilitate this process either by listing available pathways or by displaying the pathways as pdf files.

--- hypergraph/fileIO.py
+++ hypergraph/fileIO.py
@@ -1,6 +1,9 @@
 import os
 import sys
 import pandas as pd
+from Bio.KEGG.REST import kegg_info, kegg_list, kegg_get
+from Bio.KEGG.KGML import KGML_parser
+from Bio.Graphics.KGML_vis import KGMLCanvas
 
 
 def readXLSX(path):
@@ -43,16 +46,74 @@ def combineSheets(xls):
     print(combinedSheet)
     return combinedSheet
 
+
+def draw_kegg_map(path, map_id):
+    """ Render a local PDF of a KEGG map with the passed map ID
+    """
+    # Get the background image first
+    pathway = KGML_parser.read(kegg_get(map_id, "kgml"))
+    canvas = KGMLCanvas(pathway, import_imagemap=True)
+    img_filename = "%s.pdf" % map_id
+    canvas.draw(path + img_filename)
+
+
+def fetch_KGML_file(path, map_id):
+    """this function uses biopython modules to fetch the kgml file
+    associated with map_id and saves that kgml file as an xml file
+    in the specified path
     
+    Parameters
+    ----------
+    path : string
+        path to the KEGG_data folder
+    map_id : string
+        KEGG pathway identifier, eg: mtu01200 for central carbon metabolism
+    """
+    pathway = kegg_get(map_id, 'kgml')
+    kgml_text = pathway.read()
+    file_name = map_id + ".xml"
+    with open(path+file_name, 'w') as file:
+        file.write(kgml_text)
+    file.close()
+
+
+def get_possible_pathways(organism):
+    """Helper function, if we want to see possible pathways for
+    a specific organism, this function will display them along
+    with a short description
+    
+    Parameters
+    ----------
+    organism : string
+        KEGG identifier for an organism. mtu is mycobacterium tuberculosis
+        hsa is homo sapiens (human)
+    
+    Returns
+    -------
+    string
+        returns a string that holds the KEGG identifiers for all pathways
+        for the specified organism
+    """
+    pathways_text = kegg_list('pathway', organism)
+    pathways = pathways_text.read()
+
+    return pathways
+
 
 if __name__ == "__main__":
     absolutePath = os.path.abspath(__file__)
     fileDirectory = os.path.dirname(absolutePath)
     parentDirectory = os.path.dirname(fileDirectory)
-    path = os.path.join(
+    path_drug_data = os.path.join(
         parentDirectory, "input_files/Multidrug_6hr_Responses_trimmed.xlsx"
     )
 
-    print(readXLSX(path))
-    print(path)
-    combineSheets(readXLSX(path))
+    path_KEGG = os.path.join(parentDirectory, "input_files/KEGG_data/")
+
+    # print(readXLSX(path))
+    # print(path)
+    # combineSheets(readXLSX(path))
+
+    pathway_list = ['mtu01200', 'mtu00010']
+    for pathway in pathway_list:
+        fetch_KGML_file(path_KEGG, pathway)
